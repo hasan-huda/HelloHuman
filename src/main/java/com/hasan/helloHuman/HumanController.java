2ea14056179dc53c1af3d62b03cd283ddd98c7a4
Hasan Huda <hasanhuda037@gmail.com>
2023-02-06 14:15:26 -0500

    completed assignment

--- src/main/java/com/hasan/helloHuman/HumanController.java
+++ src/main/java/com/hasan/helloHuman/HumanController.java
@@ -1,5 +1,16 @@
 package com.hasan.helloHuman;
 
-public class HumanController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HumanController {
+	@RequestMapping("/")
+	public String index(@RequestParam(value="q",required=false)String searchQuery) {
+		if(searchQuery==null) {
+			return "Hello Human";
+		}
+		return "Hello "+searchQuery;
+	}
 }
